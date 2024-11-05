73e568c4dad7acefa6b95d336e0a699ba6773713
Xiao Mai <ethanmyers1995@gmail.com>
2024-11-05 17:25:14 +0000

    feat: finished

--- index.js
+++ index.js
@@ -26,7 +26,37 @@ app.get("/api/hello", function (req, res) {
 
 app.get("/api/:date?", (req, res) => {
   let date = req.params.date;
-  console.log(date)
+  let currentTime = new Date()
+  let unixDate = null;
+  let utcDate = null;
+  //check if date is valid:
+  // if (date is unix, assign to variable and use it to assign another variable converted to utc) - Convert with dateMilliseconds.toUTCString()
+  let unixRegex = /^[0-9]+$/;
+  if (unixRegex.test(date)) {
+    unixDate = date;
+    let dateMilliseconds = new Date(unixDate * 1000) //*1000 to convert to milliseconds
+    utcDate = dateMilliseconds.toUTCString();
+    
+    
+    return res.json({unix: unixDate, utc: utcDate})
+  } 
+    
+  else if (!isNaN(Date.parse(date))) {
+    unixDate = Date.parse(date);
+    utcDate = date;
+    return res.json({unix: unixDate, utc: utcDate})
+  }
+
+  else if (date == '' || date == null) {
+    unixDate = Date.parse(currentTime);
+    let dateMilliseconds = new Date(unixDate * 1000)
+    utcDate = dateMilliseconds.toUTCString();
+    return res.json({unix: unixDate, utc: utcDate})
+  }
+
+  else {
+    return res.json({error: "Invalid Date"})
+  }
 })
 
 
@@ -35,3 +65,7 @@ app.get("/api/:date?", (req, res) => {
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//converting to utc string:
+let dateNow = new Date()
+console.log(dateNow.toUTCString())
